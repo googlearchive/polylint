3020f96f9a4efef4148aed4b95fa6a04fd36bffa
AJ Ortega <ajo@google.com>
2015-08-18 10:56:41 -0700

    polish

--- test/test.js
+++ test/test.js
@@ -31,7 +31,7 @@ suite('Linter', function() {
   before(function(done) {
     polylint(testTarget, {root: path.join(__dirname, '..')}).then(function(linterWarnings){
       warnings = linterWarnings;
-      // console.log(warnings);
+     console.log(warnings);
       done();
     }).catch(function(err){
       console.log(err.stack);
