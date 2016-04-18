6d0d5dfde6452a7130b886694499950793a6e3e0
Jonathan Rajavuori <jrajav@gmail.com>
2016-04-18 13:56:25 -0500

    Rename compound-binding test component to property-not-found

--- test/linter-test.js
+++ test/linter-test.js
@@ -65,15 +65,6 @@ suite('Linter', function() {
     assert.include(warning.message, 'myVar');
   });
 
-  test('compound-binding', function() {
-    var w = findWarnings(warnings, 'compound-binding');
-    assert.equal(w.length, 1);
-    var warning = w[0];
-    assert.equal(warning.location.line, 14);
-    assert.equal(warning.location.column, 11);
-    assert.include(warning.message, 'nestedz');
-  });
-
   test('computed-binding', function() {
     var w = findWarnings(warnings, 'computed-binding');
     assert.equal(w.length, 2);
@@ -145,6 +136,15 @@ suite('Linter', function() {
     assert.equal(fourth.location.line, 29);
     assert.equal(fourth.location.column, 19);
     assert.include(fourth.message, '_brokenObserver2Changed');
+  });
+
+  test('property-not-found', function() {
+    var w = findWarnings(warnings, 'property-not-found');
+    assert.equal(w.length, 1);
+    var warning = w[0];
+    assert.equal(warning.location.line, 14);
+    assert.equal(warning.location.column, 11);
+    assert.include(warning.message, 'nestedz');
   });
 
   test('string-literals', function() {
